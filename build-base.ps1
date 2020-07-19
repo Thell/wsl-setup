bfd0cf0a8cea8c5d0c9035bbd8ae5a3420a47e00
Thell <thell@tbfowler.name>
2020-07-19 00:13:54 -0500

    build-base: add basic build runner with timer.
    Signed-off-by: Thell <thell@tbfowler.name>

--- build-base.ps1
+++ build-base.ps1
@@ -1,16 +1,23 @@
+$Distro="Ubuntu"
+
+cls;
+wsl -t $Distro
+wsl --unregister $Distro
+& $Distro
+
 $sw = New-Object System.Diagnostics.Stopwatch
 $sw.Start()
 
-Write-Host 'Global Profile' -ForegroundColor "White" -BackgroundColor "Blue"
+Write-Host 'Gobal Apt' -ForegroundColor "White" -BackgroundColor "Blue"
+wsl -d $Distro -u root -- ./scripts/00-global-apt.sh
 
-wsl -d Ubuntu -u root -- ./scripts/00-global-profile.sh
+Write-Host 'Global Profile' -ForegroundColor "White" -BackgroundColor "Blue"
+wsl -d $Distro -u root -- ./scripts/01-global-profile.sh
 
 Write-Host 'Global Bashrc' -ForegroundColor "White" -BackgroundColor "Blue"
+wsl -d $Distro -u root -- ./scripts/02-global-bashrc.sh
 
-wsl -d Ubuntu -u root -- ./scripts/01-global-bashrc.sh
-
-Write-Host 'Apt Proxy and Updates' -ForegroundColor "White" -BackgroundColor "Blue"
-
-wsl -d Ubuntu -u root -- ./scripts/02-global-apt.sh
+Write-Host 'Global GUI' -ForegroundColor "White" -BackgroundColor "Blue"
+wsl -d $Distro -u root -- ./scripts/03-global-gui.sh
 
 Write-Host 'Complete: ' + $sw.Elapsed.Duration().ToString() -ForegroundColor "White" -BackgroundColor "Blue"
