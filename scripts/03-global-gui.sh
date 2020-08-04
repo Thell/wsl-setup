5ecbabb7176dd2e2761a69f91095447008f885a9
Thell <thell@tbfowler.name>
2020-08-04 02:40:15 -0500

    global-gui: add fonts to support emojis.
    - file.show and console output work pretty good but overall
      there is still much to be done on this.
    
    Signed-off-by: Thell <thell@tbfowler.name>

--- scripts/03-global-gui.sh
+++ scripts/03-global-gui.sh
@@ -7,6 +7,7 @@ wsl -d Ubuntu -u root -- ./scripts/11-common-gui.sh
 
 It will
  - setup cascadia code availability.
+ - setup emoji font support.
  - install basic X libs.
 
 //NOTES//
@@ -22,16 +23,12 @@ curl -L -o cascadia-code.zip ${URL}
 unzip cascadia-code.zip ttf/*.ttf -d ${FONT_DIR}
 
 export DEBIAN_FRONTEND=noninteractive
-
 packages=(
   ### APT Setup
   # With recommends.
+  fonts-noto-color-emoji
+  fonts-ubuntu-console
   fontconfig
+  ttf-bitstream-vera
 )
 eatmydata apt-get -y install ${packages[@]}
-
-# packages=(
-#   ### APT Setup
-#   # Without recommends.
-# )
-# eatmydata apt-get -y --no-install-recommends install ${packages[@]}
